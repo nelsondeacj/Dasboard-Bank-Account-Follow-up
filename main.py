1dacb790a27c4930b06409bd14fa2a00013d5e71
Nelson Cruz <nelsondealcantaracj@gmail.com>
2024-01-03 16:02:24 -0300

    uploader box

--- main.py
+++ main.py
@@ -25,5 +25,24 @@ def main():
     st.write(f"Transactions: {transactions}")
     st.write(f"Updated Account Balance: ${updated_balance:.2f}")
 
+    uploaded_file = st.file_uploader("Choose a file")
+    if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
+
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
+
+
 if __name__ == "__main__":
     main()
