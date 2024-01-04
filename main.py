8682315f8c0ff21837f0e4fcc3460f3aa49c329c
Nelson Cruz <nelsondealcantaracj@gmail.com>
2024-01-03 21:29:37 -0300

    test matpltolib

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Function to simulate bank account follow-up
